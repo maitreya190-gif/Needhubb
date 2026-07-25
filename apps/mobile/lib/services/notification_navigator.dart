f99ccb1260a3f9e080adfe06f791ee5c3bf556ee
sureshkumar choudhary <sc2906484@gmail.com>
2026-07-25 17:06:59 +0530

    fix: restore missing Flutter material and Riverpod imports in notification_navigator.dart

--- apps/mobile/lib/services/notification_navigator.dart
+++ apps/mobile/lib/services/notification_navigator.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/need.dart';
 import '../models/user_state.dart';
 import '../screens/hub/conversation_screen.dart';
